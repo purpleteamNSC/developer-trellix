1e43f746960014c35652d4f84039be90bfc868cf
Purple Team <118991961+purpleteamNSC@users.noreply.github.com>
2024-05-27 12:25:24 +0000

    config libs

---
+++ Retrieve_Access_Token.py
@@ -0,0 +1,39 @@
+from dotenv import load_dotenv
+import requests
+import os
+
+load_dotenv()
+
+def get_access_token(client_id, client_secret, scope, audience):
+    url = 'https://iam.mcafee-cloud.com/iam/v1.1/token'
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+    data = {
+        'grant_type': 'client_credentials',
+        'scope': scope,
+        'audience': audience
+    }
+    auth = (client_id, client_secret)
+    
+    response = requests.post(url, headers=headers, data=data, auth=auth)
+    
+    if response.status_code == 200:
+        return response.json().get('access_token')
+    else:
+        print(f"Error: {response.status_code}")
+        return None
+
+# Exemplo de uso:
+# client_id = '<CLIENT_ID>'
+# client_secret = '<CLIENT_SECRET>'
+# scope = '<Scope required(space separated). Scope should be from the list of scope approved for the ClientType>'
+# audience = 'trellix'
+
+# access_token = get_access_token(client_id, client_secret, scope, audience)
+# if access_token:
+#     print("Access token:", access_token)
+# else:
+#     print("Failed to get access token.")
+
+api = os.getenv('TRELLIX_API_KEY')
+
+print(api)
